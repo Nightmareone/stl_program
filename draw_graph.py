10840d42f6f1aeb4ade9ec4b58c107e8395e2e95
Nightmareone <xujiongming_56789@yahoo.co.jp>
2019-07-12 14:34:09 +0900

    Update draw_graph.py

--- draw_graph.py
+++ draw_graph.py
@@ -10,13 +10,25 @@ from matplotlib import pyplot
 from mpl_toolkits import mplot3d
 
 ## モデルを表示
-def show(your_mesh):
+def show(filename_bunny):
   figure = pyplot.figure()
   axes = mplot3d.Axes3D(figure)
+
+  your_mesh = mesh.Mesh.from_file(filename_bunny)
   axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
   # 大きさを自動調整
   scale = your_mesh.points.flatten(-1)
   axes.auto_scale_xyz(scale, scale, scale)
 
+  volume, cog, inertia = your_mesh.get_mass_properties()
+  print(volume)
   # 表示
+  pyplot.rcParams["font.family"] = "IPAexGothic"
+  axes.set_xlabel("x-axis")
+  axes.set_ylabel("y-axis")
+  axes.set_zlabel("z-axis")
   pyplot.show()
+
+if __name__ == "__main__":
+  filename_bunny = 'D:\\Python\\20190613\\stl_sample\\Bunny-LowPoly.stl'
+  show(filename_bunny)
